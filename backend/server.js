ec7c44a5ad7113d53236eaf7295efdb72009d6f9
AbhishekxSrivastava <amssre.10480@gmail.com>
2025-08-09 07:18:37 +0530

    Updated the fixes

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 // --- CORS Configuration (THE FIX IS HERE) ---
 const corsOptions = {
-  origin: "http://localhost:5173", // 1. Allow only your frontend origin
+  origin: "*",
   credentials: true, // 2. Allow cookies and authorization headers
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"], // Optional: Specify allowed methods
 };
