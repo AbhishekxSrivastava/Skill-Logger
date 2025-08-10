97dc6b9c505967850c66a9836348330cff02171d
AbhishekxSrivastava <amssre.10480@gmail.com>
2025-08-10 13:24:55 +0530

    Updated some patches

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -5,7 +5,7 @@ import axios from "axios";
 
 // --- 2. Create a pre-configured Axios instance ---
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5111/api",
+  baseURL: "/api",
   withCredentials: true, // This is crucial!
 });
 
